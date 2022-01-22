5e4c26fd0ad89354fd843a1c5bca7398bcc499f5
Larissadlvi <larissadelavega.i@gmail.com>
2022-01-21 21:41:07 -0600

    terminado js

--- poo/mybooklist.js
+++ poo/mybooklist.js
@@ -12,23 +12,19 @@ Read date; can be blank, otherwise needs to be a JS Date() object
 Add to your HTML page 2 books that reference the Book objects.
 */
 class Book {
-    constructor(Title, Genre, Author, Read, Readdate){
-        this.Title = Title;
-        this.Genre = Genre;
-        this.Author = Author;
-        this.Read = Read;
-        this.Readdate = Readdate;
+    constructor(title, genre, author, read, readDate){
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+        this.read = read;
+        this.readDate = readDate;
     }
     books(){
-        return "Uno de mis libros favoritos es " + this.Title;
+        return "Uno de mis libros favoritos es " + this.title;
     }
 }
 
-let firstBook = new Book ("Los cuatro acuerdos de Miguel Ruiz");
-let secondBook = new Book ("The Carrie Diaries de Candace Bushnell");
 
-document.getElementById("book1").innerHTML = firstBook.books();
-document.getElementById("book2").innerHTML = secondBook.books();
 
 /*
 In your Javascript file - create a class BookList.
@@ -41,45 +37,80 @@ A reference to the last book read (book object) - lastbr
 An array of all the Books
 */
 
-class Booklist extends Book{
-    constructor(Title, numbooksr, bnotr, nexttbr, currentbr, lastbr, allofthebooks){
-    super(Title)
-    this.numbooksr = 4;
-    this.bnotr = 2;
-    this.nexttbr = 1;
-    this.currentbr = "El arte de amar";
-    this.lastbr = "El Alquimista";
-    this.allofthebooks = ["El diario de Anna Frank", "El Principito", "La Sonata del Nomeolvides", "Persona Normal", "Norwegian Wood", "El Alquimista", "El arte de amar"];
+class Booklist {
+    constructor(){
+    this.numbooksr = 0;
+    this.bnotr = 0;
+    this.nexttbr = null;
+    this.currentbr = null;
+    this.lastbr = null;
+    this.allofthebooks = [];
+    this.currentPosition = 0;
     }
     
 
-    libroextra1(){
-        return "El próximo libro que voy a agregar a mi lista es: " + this.numbooksr;
+    add(Book){
+        this.allofthebooks.push(Book); // aqui ya esta el arreglo
+        if (Book.read){ //libro es leído, lo suma a la cuenta de leídos
+            this.numbooksr++; 
+        }else{
+            this.bnotr++;  //de lo contrario se va a la cuenta de los no leídos
+            if (this.currentbr == null){ //si no estoy leyendo ningun libro se hace el libro actual para leer
+            this.currentbr = Book;}
+            else if (this.nexttbr == null){ //si veo que no hay libro proximo a ser leido después del anterior
+                this.nexttbr = Book; //se vuelve el libro que voy a leer cuando acabe el anterior
+            }
+            
+        }
     }
+
+    getUnreadBook(){
+        for(let i = this.currentPosition+1; i < this.allofthebooks.length; i++){ // i cuenta desde mi libro actual +1
+            if (this.allofthebooks[i].read == false){ 
+                return this.allofthebooks[i];
+            }
+        } return null; // si no encontro ningun libro que no haya sido leído regresa null : no hay
+    }
+
+    finishCurrentBook(){
+        this.currentbr.read=true; //aqui se indica que el current libro ya se leyó
+        this.currentbr.readDate= new Date (Date.now()); //aqui se le agrega la fecha en que fue leído
+        this.lastbr = this.currentbr; //agrego a mi ultimo libro leído, el que acabo de terminar
+        this.currentbr = this.nexttbr; //hago que mi libro actual sea mi proximo libro con nexttbr
+        this.nexttbr = this.getUnreadBook(); 
+        this.numbooksr++; // incrementan mis libros leídos 
+        this.bnotr--; //decrementan los que no he leído
+    } 
+
+    //agregando a mi lista cada titulo de todos mis libros
+    showBooks(){
+        for(let i =0; i < this.allofthebooks.length; i++){
+            var node = document.createElement('li');
+            node.appendChild(document.createTextNode(this.allofthebooks[i].title +" " + this.allofthebooks[i].read)); //ta leido o no
+            document.getElementById("bookList").appendChild(node);
+        
+        }
+    }
+    
 }
 
-let Booklist1 = new Book("Memorias de una Geisha");
+let firstBook = new Book ("Los cuatro acuerdos","clásico", "Miguel Ruiz", false, null );
+let secondBook = new Book ("The Carrie Diaries", "ficción", "Candace Bushnell", false, null);
 
-document.getElementById("bookList").innerHTML=Booklist1.libroextra1();
+document.getElementById("book1").innerHTML = firstBook.books();
+document.getElementById("book2").innerHTML = secondBook.books();
+
+let bookList = new Booklist();
+bookList.add(firstBook);
+bookList.add(secondBook);
+bookList.finishCurrentBook();
+bookList.showBooks();
 
 
+bookList.finishCurrentBook();
+bookList.showBooks();
 
-
-//Getter, tiene el método calcarea
 /*
-get area () {
-    return this.calcArea();
-}
-
-//Method
-
-calcArea (){
-    return this.height*this.width;
-}*/
-/*
-Every Booklist should have a few methods:
-.add(book)
-should add a book to the books list.
 .finishCurrentBook()
 should mark the book that is currently being read as "read"
 Give it a read date of new Date(Date.now())
